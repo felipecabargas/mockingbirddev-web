f84abd90905bedc33b999d68c2cafabec5407a09
A. Felipe Cabargas Madrid <felipe.cabargas@gmail.com>
2012-06-17 14:32:58 -0400

    First Commit - Only Homepage, no navbuttons

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mockingbird::Application.config.secret_token = 'db7b828cd86589ccf07430da2667f9f3a0420409349573ba172f48dabb54cc27b702e4ebe2fb3fd2c8c64b093119a7d390fc3c8113f385d7271c1015aa802214'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mockingbird::Application.config.session_store :cookie_store, key: '_mockingbird_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mockingbird::Application.config.session_store :active_record_store
