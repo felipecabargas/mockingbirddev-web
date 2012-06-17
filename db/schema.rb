d222374b2c9f5900c80d34a68fe3899200248d2e
A. Felipe Cabargas Madrid <felipe.cabargas@gmail.com>
2012-06-17 18:35:49 -0400

    Created Contact DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120617012506) do
+ActiveRecord::Schema.define(:version => 20120617223238) do
+
+  create_table "contacts", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "homes", :force => true do |t|
     t.datetime "created_at", :null => false
