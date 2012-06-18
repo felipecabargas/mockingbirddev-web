262996a23a374b72824e3160430ef545edddf184
A. Felipe Cabargas Madrid <felipe.cabargas@gmail.com>
2012-06-17 20:43:35 -0400

    ..

--- db/migrate/20120617223238_create_contacts.rb
+++ db/migrate/20120617223238_create_contacts.rb
@@ -1,9 +1,9 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
+      t.string :message
       t.string :name
       t.string :mail
-      t.string :message
 
       t.timestamps
     end
