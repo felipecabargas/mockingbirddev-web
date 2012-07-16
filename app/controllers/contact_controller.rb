948e56698178c6e231609f236ca4a4c880f5b4be
A. Felipe Cabargas Madrid <felipe.cabargas@gmail.com>
2012-07-16 03:24:24 -0400

    messages

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -8,7 +8,7 @@ class ContactController < ApplicationController
     
     if @message.valid?
       NotificationsMailer.new_message(@message).deliver
-      redirect_to(root_path, :notice => "Message was successfully sent.")
+      redirect_to(root_path, :notice => "Message was successfully sent ;)")
     else
       flash.now.alert = "Please fill all fields."
       render :new
