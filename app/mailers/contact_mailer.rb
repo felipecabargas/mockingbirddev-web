24accb27cd92fce692b17f000726cc8766d8091d
A. Felipe Cabargas Madrid <felipe.cabargas@gmail.com>
2012-08-24 23:11:22 -0400

    commented contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,22 +1,22 @@
-class ContactMailer < ActionMailer::Base
+# class ContactMailer < ActionMailer::Base
   
-  attr_accessor :name, :email, :message
+#   attr_accessor :name, :email, :message
 
-  default to: "contacto@mockingbirdev.com"
-          from: "contacto@mockingbirdev.com"
+#   default to: "contacto@mockingbirdev.com"
+#           from: "contacto@mockingbirdev.com"
 
-  headers = ('Return-Path' => 'contacto@mockingbirdev.com')
+#   headers = ('Return-Path' => 'contacto@mockingbirdev.com')
 
-  def send_email(user_info)
-    @user_info = user_info
+#   def send_email(user_info)
+#     @user_info = user_info
 
-    mail(
-      to: "contacto@mockingbirdev.com"
-      subject: "Contacto Cotizaciones y Consultas Mockingbird Dev"
-      from: "Contacto <contacto@mockingbirdev.com"
-      return_path: "contacto@mockingbirdev.com"
-      date: Time.now,
-      content_type: "text/html"
-    )
-  end
-end
+#     mail(
+#       to: "contacto@mockingbirdev.com"
+#       subject: "Contacto Cotizaciones y Consultas Mockingbird Dev"
+#       from: "Contacto <contacto@mockingbirdev.com"
+#       return_path: "contacto@mockingbirdev.com"
+#       date: Time.now,
+#       content_type: "text/html"
+#     )
+#   end
+# end
